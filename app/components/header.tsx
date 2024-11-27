e5f8c2bf5e6f0f465dcb54bfc812c98b1ab84673
Natsuki <ibron1975@gamil.com>
2024-11-28 00:53:52 +0900

    ヘッダーのリンクテキストを「MultiTool-Hub」に変更し、ホームページの見出しを「Welcome to the MultiTool-Hub」に更新

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -5,8 +5,8 @@ import Link from "next/link"; // Linkをインポート
 export default function Header() {
   return (
     <div className="container flex h-24 items-center justify-between px-8">
-      <Link href="/" className="text-lg font-semibold mt-4 p-0 hover:underline">
-        MultiTool Hub
+      <Link href="/" className="text-lg font-semibold mt-4 p-0 hover:text-muted-foreground">
+        MultiTool-Hub
       </Link>
       <ModeToggle />
     </div>
